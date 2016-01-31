e366d1dc24e85f323e8cfbc456e790b56c3d1d12
mattcoley <mattcoley@optonline.net>
2016-01-31 01:44:32 +0000

    Added a admin manager to make things a bit easier

--- oxbridgesentiment/sentiment/admin.py
+++ oxbridgesentiment/sentiment/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import TweetGroup
 
-admin.site.register(TweetGroup)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['date_added', 'name', 'positive', 'negative', 'sentiment']
+    ordering = ['date_added', 'name', 'positive', 'negative']
+    search_fields = ['name', 'date']
+
+admin.site.register(TweetGroup, TweetAdmin)
